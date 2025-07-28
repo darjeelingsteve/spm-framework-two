446703ce238b7725cf3be6b356c82476f9602fea
Stephen Anthony <steve@darjeelingapps.com>
2025-07-28 16:13:21 +0100

    Initial Commit

---
+++ FrameworkTwo/FrameworkTwo.h
@@ -0,0 +1,18 @@
+//
+//  FrameworkTwo.h
+//  FrameworkTwo
+//
+//  Created by Stephen Anthony on 28/07/2025.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FrameworkTwo.
+FOUNDATION_EXPORT double FrameworkTwoVersionNumber;
+
+//! Project version string for FrameworkTwo.
+FOUNDATION_EXPORT const unsigned char FrameworkTwoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrameworkTwo/PublicHeader.h>
+
+
