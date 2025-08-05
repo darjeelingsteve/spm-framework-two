33104d5865df123e600726094065ad9350067a7c
Stephen Anthony <steve@darjeelingapps.com>
2025-08-05 14:29:10 +0100

    Don’t pass xcconfig

--- .github/actions/xcframework-build/xcframework-build.sh
+++ .github/actions/xcframework-build/xcframework-build.sh
@@ -38,7 +38,6 @@ buildXCFramework() {
 	    -scheme "$2" \
 	    -archivePath "./xcframeworkarchives/$1/ios.xcarchive" \
 	    -sdk iphoneos \
-	    -xcconfig ./xcframework-build.xcconfig \
 	    | tee xcodebuild-raw.log \
 	    | xcbeautify --renderer github-actions
 
@@ -48,7 +47,6 @@ buildXCFramework() {
 	    -scheme "$2" \
 	    -archivePath "./xcframeworkarchives/$1/ios_sim.xcarchive" \
 	    -sdk iphonesimulator \
-	    -xcconfig ./xcframework-build.xcconfig \
 	    | tee xcodebuild-raw.log \
 	    | xcbeautify --renderer github-actions
 
