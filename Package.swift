d823393956b8cdb66808823d5fab1a3e68f3bf2d
darjeelingsteve <4540313+darjeelingsteve@users.noreply.github.com>
2025-08-06 14:31:00 +0000

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,8 +3,8 @@
 
 import PackageDescription
 
-let frameworkURL = "https://github.com/darjeelingsteve/spm-framework-two/releases/download/1.1.2/FrameworkTwo.xcframework.zip"
-let frameworkChecksum = "31e64e215ed83edd8b5a6efbaf3c427e16ceb58354ed07eb57ad8b15b0450c39"
+let frameworkURL = "https://github.com/darjeelingsteve/spm-framework-two/releases/download/1.1.3/FrameworkTwo.xcframework.zip"
+let frameworkChecksum = "814961d52f07bcde6a30512f04e9a3c014474c7dca4194d159c2e2ea4654aedf"
 
 let package = Package(
     name: "FrameworkTwo",
